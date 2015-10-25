9497499cad7f21913e85404a86de3c94394763e8
matthew weintrub <heytrub@gmail.com>
2015-10-25 12:45:09 +0000

    first commit, remove storyboard

---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Matthew Weintrub on 10/25/15.
+//  Copyright (c) 2015 matthew weintrub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+
+@end
+
